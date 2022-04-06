32b568e3c10feb87ce9da56ce048048b235835c8
RenoirTan <renoirtan2005@gmail.com>
2022-04-06 11:18:22 +0800

    main: call strlen from different file

--- asm-x86/strlen.s
+++ asm-x86/strlen.s
@@ -1,3 +1,5 @@
+%include "utils.inc"
+
 section .data
     string: db "a string of unknown length", 0xa
 
@@ -18,26 +20,3 @@ _start:
     mov ebx, 0
     mov eax, 1
     int 0x80
-
-
-strlen:
-    ; size_t strlen(char *eax)
-    ; the return value is stored in eax
-
-    ; push the value of ebx onto the stack because we are going to use this
-    ; register for some special calculations,
-    ; so its value must be preserved on the stack
-    push ebx
-    mov ebx, eax ; char *ebx = eax
-
-__strlen_iter:
-    cmp byte [eax], 0 ; if (*eax == 0)
-    jz __strlen_res ; goto __strlen_res
-    ; else
-    inc eax ; ++eax
-    jmp __strlen_iter ; continue
-
-__strlen_res:
-    sub eax, ebx ; eax -= ebx
-    pop ebx ; pop the original value of ebx from the stack into ebx
-    ret ; give control back to the caller
