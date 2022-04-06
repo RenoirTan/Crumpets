e40ba953d007206c34086678a183071bad97fa98
RenoirTan <renoirtan2005@gmail.com>
2022-04-06 11:03:53 +0800

    main: convert strlen into a function

--- asm-x86/strlen.s
+++ asm-x86/strlen.s
@@ -5,18 +5,8 @@ section .text
     global _start
 
 _start:
-    mov ebx, string ; char *ebx = string
-    mov eax, ebx ; char *eax = ebx
-
-__strlen_iter:
-    cmp byte [eax], 0 ; if (*eax == 0)
-    jz __strlen_res ; goto __strlen_res
-    ; else
-    inc eax ; ++eax
-    jmp __strlen_iter ; continue
-
-__strlen_res:
-    sub eax, ebx ; eax - ebx
+    mov eax, string ; char *eax = string
+    call strlen ; eax = strlen(eax)
 
     ; rest of _start
     mov edx, eax ; eax stores the length of the string
@@ -28,3 +18,26 @@ __strlen_res:
     mov ebx, 0
     mov eax, 1
     int 0x80
+
+
+strlen:
+    ; size_t strlen(char *eax)
+    ; the return value is stored in eax
+
+    ; push the value of ebx onto the stack because we are going to use this
+    ; register for some special calculations,
+    ; so its value must be preserved on the stack
+    push ebx
+    mov ebx, eax ; char *ebx = eax
+
+__strlen_iter:
+    cmp byte [eax], 0 ; if (*eax == 0)
+    jz __strlen_res ; goto __strlen_res
+    ; else
+    inc eax ; ++eax
+    jmp __strlen_iter ; continue
+
+__strlen_res:
+    sub eax, ebx ; eax -= ebx
+    pop ebx ; pop the original value of ebx from the stack into ebx
+    ret ; give control back to the caller
